028a973fdb5e44f7cab608cdff03a3bab170e396
Peter Madin <petermadin@yahoo.com>
2024-04-25 22:26:30 -0700

    Added unit test for database

---
+++ TCSS360-VideoGameTriviaMaze/test/Model/QuestionAnswerDatabaseTest.java
@@ -0,0 +1,50 @@
+package Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionAnswerDatabaseTest {
+    private QuestionAnswerDatabase myDatabase;
+    private Connection myConnection;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        myDatabase = new QuestionAnswerDatabase();
+        myConnection = myDatabase.getConnection();
+    }
+
+
+    @Test
+    void getConnection() {
+        if (myConnection != null) {
+           assertTrue(true, "connected");
+        }
+    }
+
+    @Test
+    void getRandomQuestion() {
+        QuestionAnswerDatabase.QuestionsAndAnswers randomQuestion = myDatabase.getRandomQuestion();
+
+        assertNotNull(randomQuestion);
+        assertNotNull(randomQuestion.getMyQuestionText());
+        assertFalse(randomQuestion.getMyAnswers().isEmpty());
+
+        // Assert that at least one answer is correct
+        boolean hasCorrectAnswer = false;
+        List<QuestionAnswerDatabase.Answer> answers = randomQuestion.getMyAnswers();
+        for (QuestionAnswerDatabase.Answer answer : answers) {
+            if (answer.isCorrect()) {
+                hasCorrectAnswer = true;
+                break;
+            }
+        }
+        assertTrue(hasCorrectAnswer, "No correct answer found for the random question");
+    }
+}
