92083130166c407907a76af75b15ab80cfaeda19
Peter Madin <petermadin@yahoo.com>
2024-04-28 22:36:54 -0700

    Added getters

--- TCSS360-VideoGameTriviaMaze/src/Model/ImageQuestion.java
+++ TCSS360-VideoGameTriviaMaze/src/Model/ImageQuestion.java
@@ -8,4 +8,23 @@ public class ImageQuestion extends MultipleChoiceQuestion{
         super(theQuestion, theAnswer);
         myImagePath = theImageFile;
     }
+
+    public String getImagePath() {
+        return myImagePath;
+    }
+
+    @Override
+    public String getType() {
+        return "Image";
+    }
+
+    @Override
+    public String getQuestion() {
+        return super.getQuestion();
+    }
+
+    @Override
+    public AnswerData getAnswers() {
+        return super.getAnswers();
+    }
 }
--- TCSS360-VideoGameTriviaMaze/src/Model/TrueFalseQuestion.java
+++ TCSS360-VideoGameTriviaMaze/src/Model/TrueFalseQuestion.java
@@ -4,4 +4,19 @@ public class TrueFalseQuestion extends MultipleChoiceQuestion {
     public TrueFalseQuestion(String theQuestion, AnswerData theAnswer) {
         super(theQuestion, theAnswer);
     }
+
+    @Override
+    public String getType() {
+        return "T/F";
+    }
+
+    @Override
+    public String getQuestion() {
+        return super.getQuestion();
+    }
+
+    @Override
+    public AnswerData getAnswers() {
+        return super.getAnswers();
+    }
 }
