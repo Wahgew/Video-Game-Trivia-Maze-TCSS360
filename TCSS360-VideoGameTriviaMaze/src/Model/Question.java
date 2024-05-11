2983db0df081d53f11d6d1a43070abaa5c4f05c8
Peter Madin <petermadin@yahoo.com>
2024-05-10 19:57:37 -0700

    Added setters for defensive programming

--- TCSS360-VideoGameTriviaMaze/src/Model/Question.java
+++ TCSS360-VideoGameTriviaMaze/src/Model/Question.java
@@ -18,38 +18,22 @@ public abstract class Question {
     /**
      * The question text.
      */
-    private final String myQuestion;
+    private String myQuestion;
 
     /**
      * The correct answer to the question.
      */
-    private final AnswerData myAnswers;
+    private AnswerData myAnswers;
 
     /**
      * The question Type (e.g., "Multi", "T/F", "Short", "Audio", "Image").
      */
-    private final String myQuestionType;
+    private String myQuestionType;
 
     /**
      * the question ID.
      */
-    private final int myID;
-//    /**
-//     * Answer option 1
-//     */
-//    private String myAnswerOption1;
-//    /**
-//     * Answer option 2
-//     */
-//    private String myAnswerOption2;
-//    /**
-//     * Answer option 3
-//     */
-//    private String myAnswerOption3;
-//    /**
-//     * Answer option 4
-//     */
-//    private String myAnswerOption4;
+    private int myID;
 
     /**
      * Question constructor that creates a question with a question text,
@@ -59,34 +43,11 @@ public abstract class Question {
      * @param theAnswer the data representing the answer choices and correct index
      */
     protected Question(final String theQuestion, final AnswerData theAnswer, final String theType, final int theID) {
-        myQuestion = theQuestion;
-        myAnswers = theAnswer;
-        myQuestionType = theType;
-        myID = theID;
+        setQuestion(theQuestion);
+        setAnswers(theAnswer);
+        setQuestionType(theType);
+        setQuestionID(theID);
     }
-
-//    /**
-//     * Constructor for initialize the fields of the question.
-//     * @param theQuestion
-//     * @param theAnswer
-//     * @param theOption1
-//     * @param theOption2
-//     * @param theOption3
-//     * @param theOption4
-//     */
-//    public Question(final String theQuestion, final AnswerData theAnswer, String myQuestionType, final String theOption1,
-//                    final String theOption2, final String theOption3, final String theOption4) {
-//        this.myQuestionType = myQuestionType;
-//        if (theQuestion == null || theAnswer == null || theOption1 == null || theOption2 == null) {
-//            throw new IllegalArgumentException("Question and Answer are required and cannot be null");
-//        }
-//        myQuestion = theQuestion;
-//        myAnswers = theAnswer;
-//        myAnswerOption1 = theOption1;
-//        myAnswerOption2 = theOption2;
-//        myAnswerOption3 = theOption3;
-//        myAnswerOption4 = theOption4;
-//    }
 
     //TODO: For now this just checks the answer of user input
     // we will need to make check for the four button option the user clicks on.
@@ -134,38 +95,34 @@ public abstract class Question {
         return myID;
     }
 
-    //    /**
-//     * Getter for answer option 1
-//     *
-//     * @return the answer option
-//     */
-//    public String getAnswerOption1() {
-//        return myAnswerOption1;
-//    }
-//    /**
-//     * Getter for answer option 2
-//     *
-//     * @return the answer option 2
-//     */
-//    public String getAnswerOption2() {
-//        return myAnswerOption2;
-//    }
-//    /**
-//     * Getter for answer option 3
-//     *
-//     * @return the answer option 3
-//     */
-//    public String getAnswerOption3() {
-//        return myAnswerOption3;
-//    }
-//    /**
-//     * Getter for answer option 4
-//     *
-//     * @return the answer option 4
-//     */
-//    public String getAnswerOption4() {
-//        return myAnswerOption4;
-//    }
+    public void setQuestion(final String theQuestion) {
+         if (theQuestion == null) {
+             throw new IllegalArgumentException("Question can't be null");
+         }
+
+         myQuestion = theQuestion;
+    }
+
+    public void setAnswers(final AnswerData theAnswer) {
+        if (theAnswer == null) {
+            throw new IllegalArgumentException("Answers cannot be null");
+        }
+        myAnswers = theAnswer;
+    }
+
+    public void setQuestionType(final String theType) {
+        if (theType == null || theType.isEmpty()) {
+            throw new IllegalArgumentException("Question type cannot be null or empty");
+        }
+        myQuestionType = theType;
+    }
+
+    public void setQuestionID(final int theID) {
+        if (theID <= 0) {
+            throw new IllegalArgumentException("Question ID must be a positive integer");
+        }
+        myID = theID;
+    }
 
     /**
      * toString displaying question, possible answer with
