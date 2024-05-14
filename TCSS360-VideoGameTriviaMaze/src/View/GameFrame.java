c22cab26b8fbaed58e1dd5fb4eb09370583b1caf
Peter Madin <petermadin@yahoo.com>
2024-05-14 00:50:36 -0700

    Switched panels to MovementButton to test Question UI

--- TCSS360-VideoGameTriviaMaze/src/View/GameFrame.java
+++ TCSS360-VideoGameTriviaMaze/src/View/GameFrame.java
@@ -58,7 +58,11 @@ public class GameFrame extends JFrame {
         leftPanel.setBackground(Color.GRAY);
         centerPanel.setBackground(Color.GRAY);
         // Add left and center panels to main panel
-        mainPanel.add(myGamePanel.createLayeredPanel(), BorderLayout.WEST);
+        //mainPanel.add(myGamePanel.createLayeredPanel(), BorderLayout.WEST);
+
+        //TODO: This main panel here is testing for seeing question pop up UIs
+        mainPanel.add(new MovementButtonPanel(theGamePanel), BorderLayout.WEST);// comment this out and uncomment above to switch back to other UI
+
         mainPanel.add(centerPanel, BorderLayout.CENTER);
         leftPanel.setPreferredSize(new Dimension(200,200));
 
