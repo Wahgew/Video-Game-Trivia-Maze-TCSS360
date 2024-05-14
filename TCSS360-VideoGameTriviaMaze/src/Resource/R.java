5c497856d5312982ec1f11c81034803c09b3d31e
Peter Madin <petermadin@yahoo.com>
2024-05-14 00:45:52 -0700

    Created resource class for colors

---
+++ TCSS360-VideoGameTriviaMaze/src/Resource/R.java
@@ -0,0 +1,29 @@
+package Resource;
+
+import java.awt.*;
+/**
+ * Resource class providing color constants for UI elements.
+ * @author Peter Madin
+ * @version 0.0.1 December 7, 2023
+ */
+public class R {
+    /**
+     * Inner static class containing color constants for various UI elements.
+     */
+    public static class Colors {
+        /** Color for outlines or borders in UI elements. */
+        public static final Color OUTLINES = new Color(47, 47, 47);
+
+        /** Text label color for UI elements. */
+        public static final Color TEXT_LABEL = new Color(255,207,64);
+
+        /** Panel color for popUpUI elements. */
+        public static final Color QUESTION_PANEL_BG = new Color(70, 69, 69);
+
+        /** Color for displaying numbers in UI elements. */
+        public static final Color NUMBERS = new Color(255, 182, 18);
+
+        /** Header color for UI elements. */
+        public static final Color HEADER = new Color(133, 41, 40);
+    }
+}
