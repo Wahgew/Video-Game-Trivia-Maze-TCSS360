9bba7ead9d8698820035bd528fb0686fed78f92f
Peter Madin <petermadin@yahoo.com>
2024-05-18 01:43:57 -0700

    Fixed player load and save data

--- TCSS360-VideoGameTriviaMaze/src/Model/GameDataManger.java
+++ TCSS360-VideoGameTriviaMaze/src/Model/GameDataManger.java
@@ -3,6 +3,7 @@ package Model;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,42 +13,45 @@ public class GameDataManger {
     private int myPlayerHealth;
     private int myPlayerScore;
     private int myScoreMultiplier;
-    private int myCorrectCount;
-    private int myIncorrectCount;
+    private int myCorrectTotal;
+    private int myIncorrectTotal;
     private HashMap<Integer, Boolean> myAnsweredQuestion;
+    private static final String SAVE_FILE_PATH = "src/Resource/save.json";
 
     @JsonCreator
-    public GameDataManger(@JsonProperty("playerHealth") int thePlayerHealth,
-                          @JsonProperty("playerScore") int thePlayerScore,
-                          @JsonProperty("playerMultiplier") int thePlayerMulti,
-                          @JsonProperty("correctCount") int theCorrectCount,
-                          @JsonProperty("incorrectCount") int theIncorrectCount,
-                          @JsonProperty("answeredQuestions") HashMap<Integer, Boolean> theAnsweredQuestions) {
-        myPlayerHealth = thePlayerHealth;
-        myPlayerScore = thePlayerScore;
-        myScoreMultiplier = thePlayerMulti;
-        myCorrectCount = theCorrectCount;
-        myIncorrectCount = theIncorrectCount;
-        myAnsweredQuestion = theAnsweredQuestions;
-    }
+    //@JsonDeserialize(as = GameDataManger.class)
+//    public GameDataManger(@JsonProperty("playerHealth") int thePlayerHealth,
+//                          @JsonProperty("playerScore") int thePlayerScore,
+//                          @JsonProperty("playerMultiplier") int thePlayerMulti,
+//                          @JsonProperty("correctCount") int theCorrectCount,
+//                          @JsonProperty("incorrectCount") int theIncorrectCount,
+//                          @JsonProperty("answeredQuestions") HashMap<Integer, Boolean> theAnsweredQuestions) {
+//        myPlayerHealth = thePlayerHealth;
+//        myPlayerScore = thePlayerScore;
+//        myScoreMultiplier = thePlayerMulti;
+//        myCorrectTotal = theCorrectCount;
+//        myIncorrectTotal = theIncorrectCount;
+//        myAnsweredQuestion = theAnsweredQuestions;
+//    }
 
-    public static void saveGameData(GameDataManger theGameData, File theSaveFile) {
+    public void saveGameData() {
         ObjectMapper om = new ObjectMapper();
-
+        File saveFile = new File(SAVE_FILE_PATH);
         try {
-            om.writeValue(theSaveFile, theGameData);
+            om.writeValue(saveFile, Player.getInstance());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static GameDataManger loadGameData(File saveFile) {
+    public void loadGameData() {
         ObjectMapper objectMapper = new ObjectMapper();
+        File loadFile = new File(SAVE_FILE_PATH);
+
         try {
-            return objectMapper.readValue(saveFile, GameDataManger.class);
+            objectMapper.readValue(loadFile, Player.class);
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
     }
 }
