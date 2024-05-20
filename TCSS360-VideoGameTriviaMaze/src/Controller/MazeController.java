3e595afb3bea871377b8a566be48b9c382b5a465
Peter Madin <petermadin@yahoo.com>
2024-05-19 19:04:24 -0700

    Fixed handlePlayerMovement to ask questions before moving

--- TCSS360-VideoGameTriviaMaze/src/Controller/MazeController.java
+++ TCSS360-VideoGameTriviaMaze/src/Controller/MazeController.java
@@ -31,34 +31,43 @@ public class MazeController {
 //        }
     }
 
-    /**
-     * Called when "New Game" button is pressed on Main Menu
-     * TEMPORARY IMPLEMENTATION, JUST TO CHECK FUNCTIONALITY OF MODEL.
-     * @param theMazeFrame the underlying GameFrame
-     */
-    public static void promptAnswer(GameFrame theMazeFrame ) {
-        while (theMazeFrame.getGamePanelFocus()) { // THIS WHILE LOOP IS ONLY FOR TESTING CURRENTLY TODO: REMOVE THIS WHEN NOT NEEDED
-            String movement = JOptionPane.showInputDialog(Maze.getInstance() + "\n"
-                    + Maze.getInstance().getMyRoom(Player.getInstance().getMyLocationRow(),
-                    Player.getInstance().getMyLocationCol()));
-            Player.getInstance().movePlayer(Integer.parseInt(movement));
-            theMazeFrame.getMyGamePanel().updateButtonStatus();
-        }
-    }
+//    /**
+//     * Called when "New Game" button is pressed on Main Menu
+//     * TEMPORARY IMPLEMENTATION, JUST TO CHECK FUNCTIONALITY OF MODEL.
+//     * @param theMazeFrame the underlying GameFrame
+//     */
+//    public static void promptAnswer(GameFrame theMazeFrame ) {
+//        while (theMazeFrame.getGamePanelFocus()) { // THIS WHILE LOOP IS ONLY FOR TESTING CURRENTLY TODO: REMOVE THIS WHEN NOT NEEDED
+//            String movement = JOptionPane.showInputDialog(Maze.getInstance() + "\n"
+//                    + Maze.getInstance().getMyRoom(Player.getInstance().getMyLocationRow(),
+//                    Player.getInstance().getMyLocationCol()));
+//            Player.getInstance().movePlayer(Integer.parseInt(movement));
+//            theMazeFrame.getMyGamePanel().updateButtonStatus();
+//        }
+//    }
 
-    public void handlePlayerMovement(Direction direction, GamePanel gamePanel) {
+    public void handlePlayerMovement(Direction theDirection, GamePanel theGamePanel) {
         Player player = Player.getInstance();
         int playerRow = player.getMyLocationRow();
         int playerCol = player.getMyLocationCol();
-        Door door = Maze.getInstance().getMyRoom(playerRow, playerCol).getMyDoor(direction);
+        player.setMyDirection(theDirection);
+        Door door = Maze.getInstance().getMyRoom(playerRow, playerCol).getMyDoor(theDirection);
+
+        if (door != null && !door.getMyAttemptStatus()) {
+            displayQuestionPanel(door, theGamePanel);
+        } else {
+            player.movePlayer(theDirection);
+        }
+
         // Door is unlocked or question not attempted, allow movement
-        player.movePlayer(direction);
+        //player.movePlayer(direction); // check player move attempt move: first gets questions correctness then move.
+        // TODO: ask the questions, then you call question attempted. based on the correctess -> allow them to move call player.move();
 
         // Check if the move was towards a door
-        if (!door.getMyAttemptStatus()) {
-            // Door hasn't been attempted, display the question panel
-            displayQuestionPanel(door, gamePanel);
-        }
+//        if (!door.getMyAttemptStatus()) {
+//            // Door hasn't been attempted, display the question panel
+//            displayQuestionPanel(door, gamePanel);
+//        }
 
         try { // TODO: THIS IS FOR TESTING, remove when Room image is properly added.
             BufferedImage mapImage = ImageIO.read(new File(Maze.getInstance().getMyRoom(Player.getInstance().getMyLocationRow(), Player.getInstance().getMyLocationCol()).getRoomFileName()));
@@ -71,7 +80,7 @@ public class MazeController {
     private void displayQuestionPanel(Door door, GamePanel gamePanel) {
         SwingUtilities.invokeLater(() -> {
             // Create and display the question panel
-            QuestionPanel questionPanel = new QuestionPanel(door ,gamePanel); //TODO: if error just ignore the null
+            new QuestionPanel(door ,gamePanel); //TODO: if error just ignore the null
             //questionPanel.popUpUI();
         });
     }
