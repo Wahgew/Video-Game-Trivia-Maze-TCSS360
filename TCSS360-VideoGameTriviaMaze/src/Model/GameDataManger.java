3d5b13ded4b2aa5e1036418e7c04dcc4ab5ffb41
Peter Madin <petermadin@yahoo.com>
2024-05-20 15:18:13 -0700

    Removed constructor calls a default one instead

--- TCSS360-VideoGameTriviaMaze/src/Model/GameDataManger.java
+++ TCSS360-VideoGameTriviaMaze/src/Model/GameDataManger.java
@@ -1,38 +1,12 @@
 package Model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class GameDataManger {
-    private int myPlayerHealth;
-    private int myPlayerScore;
-    private int myScoreMultiplier;
-    private int myCorrectTotal;
-    private int myIncorrectTotal;
-    private HashMap<Integer, Boolean> myAnsweredQuestion;
     private static final String SAVE_FILE_PATH = "src/Resource/save.json";
-
-    @JsonCreator
-    //@JsonDeserialize(as = GameDataManger.class)
-//    public GameDataManger(@JsonProperty("playerHealth") int thePlayerHealth,
-//                          @JsonProperty("playerScore") int thePlayerScore,
-//                          @JsonProperty("playerMultiplier") int thePlayerMulti,
-//                          @JsonProperty("correctCount") int theCorrectCount,
-//                          @JsonProperty("incorrectCount") int theIncorrectCount,
-//                          @JsonProperty("answeredQuestions") HashMap<Integer, Boolean> theAnsweredQuestions) {
-//        myPlayerHealth = thePlayerHealth;
-//        myPlayerScore = thePlayerScore;
-//        myScoreMultiplier = thePlayerMulti;
-//        myCorrectTotal = theCorrectCount;
-//        myIncorrectTotal = theIncorrectCount;
-//        myAnsweredQuestion = theAnsweredQuestions;
-//    }
 
     public void saveGameData() {
         ObjectMapper om = new ObjectMapper();
