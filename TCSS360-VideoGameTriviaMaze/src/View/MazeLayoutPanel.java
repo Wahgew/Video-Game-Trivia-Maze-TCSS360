5475308bb379abb200f1c3441029346c290200d2
Peter Madin <petermadin@yahoo.com>
2024-05-25 17:03:44 -0700

    Comment update

--- TCSS360-VideoGameTriviaMaze/src/View/MazeLayoutPanel.java
+++ TCSS360-VideoGameTriviaMaze/src/View/MazeLayoutPanel.java
@@ -1,16 +1,18 @@
 package View;
 
 import Model.Maze;
+import Model.Player;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MazeLayoutPanel extends JPanel { // TODO: this should pop up on "New Game" Button, check WelcomeScreen?
+    private GameFrame myGameFrame;
     private final JButton myLayoutButton1;
     private final JButton myLayoutButton2;
     private final JButton myLayoutButton3;
 
-    public MazeLayoutPanel() {
+    public MazeLayoutPanel(GameFrame theFrame) {
         myLayoutButton1 = new JButton("Max Doors Layout"); // TODO: add pictures of layouts?
         myLayoutButton2 = new JButton("Mazey Maze Layout");
         myLayoutButton3 = new JButton("Zig-Zag Layout");
@@ -19,17 +21,28 @@ public class MazeLayoutPanel extends JPanel { // TODO: this should pop up on "Ne
         add(myLayoutButton1);
         add(myLayoutButton2);
         add(myLayoutButton3);
+
+        myGameFrame = theFrame;
         setupButtons();
     }
     private void setupButtons() {
+        //GamePanel gamePanel = new GamePanel();
         myLayoutButton1.addActionListener(e -> {
-            Maze.getInstance("MaxDoorsLayout.txt");
+            Maze.resetMaze("MaxDoorsLayout.txt");
+            Player.resetPlayer();
+            myGameFrame.switchToGamePanel(new GamePanel());
         });
         myLayoutButton2.addActionListener(e -> {
-            Maze.getInstance("MazeyMazeLayout.txt");
+            //Maze.getInstance("MazeyMazeLayout.txt");
+            Maze.resetMaze("MazeyMazeLayout.txt");
+            Player.resetPlayer();
+            myGameFrame.switchToGamePanel(new GamePanel());
         });
         myLayoutButton3.addActionListener(e -> {
-            Maze.getInstance("ZigZagLayout.txt");
+            //Maze.getInstance("ZigZagLayout.txt");
+            Maze.resetMaze("ZigZagLayout.txt");
+            Player.resetPlayer();
+            myGameFrame.switchToGamePanel(new GamePanel());
         });
     }
 }
