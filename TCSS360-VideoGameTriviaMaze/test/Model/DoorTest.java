d2227933530ed3941688b1a53e540b0702568bb5
Sopheanith3 <nsopheanith3@gmail.com>
2024-06-07 00:12:41 -0700

    JUnit for Door test class.

---
+++ TCSS360-VideoGameTriviaMaze/test/Model/DoorTest.java
@@ -0,0 +1,59 @@
+package Model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DoorTest {
+    private Door door;
+
+    @BeforeEach
+    void setUp() {
+        door = new Door(Direction.NORTH);
+    }
+
+    @Test
+    void getMyLockStatus_DefaultValue_True() {
+        assertTrue(door.getMyLockStatus());
+    }
+
+    @Test
+    void getMyAttemptStatus_DefaultValue_False() {
+        assertTrue(!door.getMyAttemptStatus());
+    }
+
+    @Test
+    void getMyLeadsOutOfBounds_DefaultValue_False() {
+        assertTrue(!door.getMyLeadsOutOfBounds());
+    }
+
+    @Test
+    void getMyMovementIcon_DefaultDirection_ReturnsExpectedIcon() {
+        assertEquals("/Resource/upLocked.png", door.getMyMovementIcon());
+    }
+
+    @Test
+    void setMyLockStatus_ChangedValue_ReturnsChangedValue() {
+        door.setMyLockStatus(false);
+        assertTrue(!door.getMyLockStatus());
+    }
+
+    @Test
+    void setMyAttemptStatus_ChangedValue_ReturnsChangedValue() {
+        door.setMyAttemptStatus(true);
+        assertTrue(door.getMyAttemptStatus());
+    }
+
+    @Test
+    void toString_DefaultDoor_ReturnsStringRepresentation() {
+        String expectedString = "Door{myLockStatus=true, myAttemptStatus=false, myLeadsOutofBounds=false}";
+        assertEquals(expectedString, door.toString());
+    }
+
+//    @Test
+//    void askQuestion_DefaultDoor_ReturnsQuestion() {
+//    }
+
+}
